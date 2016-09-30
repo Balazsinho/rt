168378ebc4e8e5ad1b264d02a1e50ee69b3aad7c
Kisgep <Kisgep>
2016-09-30 12:18:14 +0100

    Changed DB engine to MySQL

--- rovidtav/settings.py
+++ rovidtav/settings.py
@@ -87,13 +87,25 @@ WSGI_APPLICATION = 'rovidtav.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# =============================================================================
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+# =============================================================================
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rovidtav',
+        'USER': 'rovidtav',
+        'PASSWORD': 'R0v1dt4v',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
